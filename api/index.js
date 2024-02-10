544fb81edd925ac6c82ffd54f83a968a99c75024
ParthSankpal <parth.sankpal47@gmail.com>
2024-02-10 23:27:33 +0530

    removed dist from index.js

--- api/index.js
+++ api/index.js
@@ -30,10 +30,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/banker', bankerRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client',  'index.html'));
 })
 
 
