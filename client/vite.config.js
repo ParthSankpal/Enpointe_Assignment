817cc4883c1aeba8bd475f084ba403aade94c898
ParthSankpal <114609530+ParthSankpal@users.noreply.github.com>
2024-02-11 12:17:43 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:  'enpointeassignment-production-3a0d.up.railway.app',  //Should be kept in .env
+        target:  'http://enpointeassignment-production-3a0d.up.railway.app',  //Should be kept in .env
         //localhost:3002 //enpointeassignment-production-922f.up.railway.app
         secure:false,
       },
