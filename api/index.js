f8fd41978aa858d8ad3aba41598a1567efe2866e
ParthSankpal <parth.sankpal47@gmail.com>
2024-02-13 20:44:30 +0530

    Going back 3 commits

--- api/index.js
+++ api/index.js
@@ -32,6 +32,7 @@ app.use('/api/banker', bankerRouter);
 
 app.use(express.static(path.join(__dirname, '/client')));
 
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client',  'index.html'));
 })
